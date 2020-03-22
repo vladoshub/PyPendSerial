2793d7ea69f8fea1967c7f6dc67cb1e5f8b7c069
vladoshub <38497674+vladoshub@users.noreply.github.com>
2020-03-22 17:26:57 +0300

    Create measurement.py

---
+++ measurement.py
@@ -0,0 +1,32 @@
+class measurement:
+
+    Status=None
+    Time=None
+    Coordinate=None
+    Count=None
+    Valid=None
+
+    def set_Status(self,status):
+        self.status=status
+    def get_Status(self):
+         return self.status
+
+    def set_Time(self,Time):
+        self.Time=Time
+    def get_Time(self):
+        return self.Time
+
+    def set_Coordinate(self,Coordinate):
+        self.Coordinate=Coordinate
+    def get_Coordinate(self):
+        return self.Coordinate
+
+    def set_Count(self,Count):
+        self.Count=Count
+    def get_Count(self):
+        return self.Count
+
+def set_Valid(self,Valid):
+    self.Valid=Valid
+def get_Valid(self):
+    return self.Valid
